c5f4baf09a8e3667d089d188238749e6957d1f0e
v-vermadee <v-vermadee@microsoft.com>
2024-02-27 09:56:45 +0530

    fetch bus info, for user and admin

--- src/main/java/com/travellers_apis/nomadic_bus/repositories/BusRepository.java
+++ src/main/java/com/travellers_apis/nomadic_bus/repositories/BusRepository.java
@@ -1,5 +1,7 @@
 package com.travellers_apis.nomadic_bus.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.travellers_apis.nomadic_bus.models.Bus;
 
 @Repository
 public interface BusRepository extends JpaRepository<Bus, Integer> {
+
+    List<Bus> findByBusType(String busType);
 
 }
